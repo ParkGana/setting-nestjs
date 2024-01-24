ccec227d8581bbf27ecefb24ad5372554d6cb286
ParkGana <gnchoco97@gmail.com>
2024-01-24 12:13:28 +0900

    nest.js 기초 설정

--- README.md
+++ README.md
@@ -12,5 +12,12 @@ npm run start
 
 ##### [ nest.js 프로젝트 생성 ]
 
-npx @nestjs/cli@8.0 new .
+npx @nestjs/cli@8.0 new .  
 npm install typescript@4.7.4
+
+<br />
+
+##### [ nest.js 기초 설정 ]
+
+npm install @nestjs/swagger@5 swagger-ui-express --save  
+npm install pg@8.7.1 typeorm@0.2.45 @nestjs/typeorm@8.0.1 --save
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common'
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+    const port = 3000
+
+    const app = await NestFactory.create(AppModule)
+
+    /* cors 설정 */
+    app.enableCors()
+
+    /* swagger 설정 */
+    const swaggerConfig = new DocumentBuilder().setTitle('API').build()
+    const document = SwaggerModule.createDocument(app, swaggerConfig)
+    SwaggerModule.setup('swagger', app, document)
+
+    await app.listen(port)
+
+    Logger.verbose(`Application running on port ${port}`)
 }
-bootstrap();
+bootstrap()
